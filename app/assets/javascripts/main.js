2c5396361bc64e3c39c99887e276524bfa345c6f
Mitchell Hollander <MitchellSH@live.com>
2019-07-21 18:31:51 -0500

    its been a hard day's night and ive been working like a dog, i should be sleeping like a log

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,27 @@
-function nextCrypto(elm) {
-    const card = document.querySelector('.selected');
-    let parent = card.parentElement;
-    parent.classList.add('col-md-4');
-    let index = parseInt(card.getAttribute('data-index')) + 1;
-    if(index > 6) index = 0;
-    let next = document.querySelector(`.card[data-index="${index}"]`);
-    card.classList.add('muted');
-    card.classList.remove('selected');
-    parent.parentElement.appendChild(parent);
-    next.classList.remove('muted');
-    next.classList.add('selected');
-    next.parentElement.classList.remove('col-md-4');
+function selectCrypto (elm) {
+    if(elm.classList.contains('inactive')) {
+        const currentActiveElm = document.querySelector('.active');
+        elm.classList.remove('inactive');
+        currentActiveElm.classList.remove('active');
+        elm.classList.add('active');
+        currentActiveElm.classList.add('inactive');
+        elm.parentElement.classList.remove('col-md-4');
+        currentActiveElm.parentElement.classList.add('col-md-4');
+        elm.parentElement.parentElement.insertBefore(elm.parentElement, currentActiveElm.parentElement);
+        currentActiveElm.parentElement.parentElement.appendChild(currentActiveElm.parentElement);
+    } else {
+        const nextActiveElm = elm.parentElement.parentElement.children[1].firstElementChild;
+        elm.classList.remove('active');
+        nextActiveElm.classList.remove('inactive');
+        elm.classList.add('inactive');
+        nextActiveElm.classList.add('active');
+        elm.parentElement.parentElement.appendChild(elm.parentElement);
+        nextActiveElm.parentElement.classList.remove('col-md-4');
+        elm.parentElement.classList.add('col-md-4');
+    }
+    window.scrollTo(0, 0);
 }
 
 window.addEventListener('keydown', (e) => {
-    if(e.keyCode === 32) nextCrypto();
+    if(e.keyCode === 32) selectCrypto(document.querySelector('.active')); // Space Key
 });
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,21 +4,15 @@ class DashboardController < ApplicationController
 
   def index
     coins = []
-    hashes = []
-    Cryptocompare::Price.full(['BTC', 'ETH', 'LTC', 'ETC', 'BTH', 'BCH', 'BAT'], 'USD').each do |hash|
-      hashes << hash[1]
-    end
-    hashes[0].each do |sub|
-      coin = Hash.new
-      sub[1].each do |h|
-        coin["Name"] = sub[0]
-        coin["Details"] = h[1]
+    Cryptocompare::Price.full(['BTC', 'ETH', 'LTC', 'ETC', 'BTH', 'TRX', 'BAT', 'REQ', 'XRP', 'EOS'], 'USD')["DISPLAY"].map do |hash|
+      hash[1].each do |sub|
+        coin = Hash.new
+        sub.each do |h|
+          coin["Name"] = hash[0]
+          coin["Details"] = h
+        end
+        coins << coin
       end
-      fromVolume = coin["Details"]["TOTALVOLUME24H"].to_i
-      toVolume = coin["Details"]["TOTALVOLUME24HTO"].to_i
-      coin["Details"]["TOTALVOLUMEPERC24H"] = (((toVolume.to_f - fromVolume.to_f) / toVolume.to_f) * 100).round
-      coin["Details"]["CHANGEPCTDAY"] = coin["Details"]["CHANGEPCTDAY"] === 0 ? 0 : coin["Details"]["CHANGEPCTDAY"].truncate(2).to_f
-      coins << coin
     end
     @coins = coins
   end
